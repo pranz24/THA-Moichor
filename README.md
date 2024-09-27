f14815224e06f95cc9e9b4fa41b508fea8ff03ed
Pranjal Tandon <pranjal.tandon@gmail.com>
2024-09-26 23:17:17 -0700

    Improve Readme & code fix

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 # Moichor Assignment
-This repository contains a Python script designed to handle DICOM images by fetching metadata, updating tags in the images, uploading them to an API endpoint, and triggering analytics on the uploaded images. The script employs robust error handling and logging mechanisms, including exponential backoff for retrying API requests.
+Python script designed to handle DICOM images by fetching metadata, updating tags in the images, uploading them to an API endpoint, and triggering analytics on the uploaded images.
+
 
 ### Features
+* Fetch metadata from a configurable API endpoint.
+* Verify and update DICOM image tags with metadata.
+* Upload DICOM images to a given API.
+* Trigger analytics after successful uploads.
+* Exponential backoff and retry mechanism for handling API failures.
+* Logging for troubleshooting and monitoring.
 
-    - Fetch metadata from a configurable API endpoint.
-    - Verify and update DICOM image tags with metadata.
-    - Upload DICOM images to a given API.
-    - Trigger analytics after successful uploads.
-    - Exponential backoff and retry mechanism for handling API failures.
-    - Extensive logging for troubleshooting and monitoring.
+### Considerations
+* **Log Submission on Failure**: Add an API endpoint to automatically send log files when the program encounters an error or fails, providing useful data for debugging and monitoring.
+* **Atomic Upload**: Enable atomic uploads by zipping the entire folder of DICOM images and sending it as a single package, ensuring that either all or none of the files are uploaded, preventing partial uploads. 
+* **Concurrent DICOM Image Upload**: Implement the ability to upload DICOM images concurrently using asyncio and aiohttp to improve performance and reduce overall upload time. (Not required if using atomic upload - performance gains might not be much)
+* **Unit Tests**: Add unit test to make sure each function behaves as expected after updates.
 
 ### Installation
 
 1. Clone the repository:
 ```
-git clone <repository-url>
-cd <repository-directory>
+git clone https://github.com/pranz24/THA-Moichor.git
+cd THA-Moichor
 ```
 
 2. Install the required Python packages using `requirements.txt`:
@@ -24,6 +30,20 @@ pip install -r requirements.txt
 ```
 
 3. Set the required environment variables:
+
+| Variable     |                     Description                        |
+| ------------ | ------------------------------------------------------ |
+| `LOG_LEVEL`  | Specifies the logging level (e.g., DEBUG, INFO, ERROR) |
+| `MD_URL`     |              The URL for the Metadata API              |
+| `MD_KEY`     |        The API key for authenticating Metadata API     |
+| `U_URL`      |              The URL for the Upload API                |
+| `U_KEY`      |        The API key for authenticating Upload API       |
+| `A_URL`      |              The URL for the Analytics API             |
+| `A_KEY`      |       The API key for authenticating Analytics API     |
+
+
+
+
 Example (Linux/macOS):
 ```
 export MD_URL="https://example.com/metadata"
@@ -44,17 +64,35 @@ set A_URL="https://example.com/analytics"
 set A_KEY="your_analytics_api_key"
 ```
 
+Or create a `.env` file:
+```
+LOG_LEVEL="INFO"
+MD_URL="https://example.com/metadata"
+MD_KEY="your_metadata_api_key"
+U_URL="https://example.com/upload"
+U_KEY="your_upload_api_key"
+A_URL="https://example.com/analytics"
+A_KEY="your_analytics_api_key"
+
+```
+
+
 ### Usage
 
 To run the script, use the following command. Make sure the DICOM images you want to process are stored in the specified directory.
 ```
-python main.py --sample_id <SAMPLE_ID> --backoff_attempts <RETRY_COUNT> --attempt_number <ATTEMPT_NO> --directory <DICOM_DIRECTORY> --console_logs <CONSOLE_LOGS>
+python main.py --sample_id <SAMPLE_ID> --max_retry <RETRY_COUNT> --attempt_number <ATTEMPT_NO> --directory <DICOM_DIRECTORY> --console_logs <CONSOLE_LOGS>
 ```
+
+|     Argument       | Type |  Default Value    |                                      Description                                        |
+| ------------------ | ---- | ----------------- | --------------------------------------------------------------------------------------- |
+|`--sample_id`       | str  | ref0000022tes-cbc |                Sample ID used to fetch metadata and update DICOM tags                   |
+|`--backoff_attempts`| int  |         2         |              Number of retries before declaring failure when calling APIs               |
+|`--attempt_number`	 | int  |         1         |    Custom tag used for PatientID field in DICOM images: <sample_id>-<attempt_number>    |
+|`--directory`       | str  |     ./data/       | Path to the directory containing DICOM images. (Giving absolute path is a good practice |
+|`--console_logs`    | bool |       False       |       Set to True to print logs to the console in addition to writing to log file       |
+
 
 ### Logging
 
-Logs are stored in `moichor.log` by default. You can enable console logging with the ```--console_logs True``` argument to display logs on the terminal.
-
-## Resources Used
-1. [Reading & Editing DICOM Metadata with Python](https://medium.com/@ashkanpakzad/reading-editing-dicom-metadata-w-python-8204223a59f6)
-2. [RESTful API Testing with PyTest: A Complete Guide](https://laerciosantanna.medium.com/mastering-restful-api-testing-with-pytest-56d22460a9c4)
+Logs are stored in `<SAMPLE_ID>-<ATTEMPT_NO>.log` by default. You can enable console logging with the ```--console_logs True``` argument to display logs on the terminal.
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import time
 import logging
 import pydicom
 import requests
@@ -11,7 +12,7 @@ from dotenv import load_dotenv
 
 # Load the .anv file
 load_dotenv()
-
+LOG_LEVEL = os.getenv('LOG_LEVEL')
 METADATA_URL = os.getenv('MD_URL')
 METADATA_KEY = os.getenv('MD_KEY')
 UPLOAD_URL = os.getenv('U_URL')
@@ -19,31 +20,47 @@ UPLOAD_KEY = os.getenv('U_KEY')
 ANALYTICS_URL = os.getenv('A_URL')
 ANALYTICS_KEY = os.getenv('A_KEY')
 
+"""
+Update tags in all dicom images with fetched metadata.
+Get Image tags -> Update tags if needed -> overwrite the existing image with updated tags
 
+Tag References:
+https://dicom.innolitics.com/ciods
+https://www.dicomlibrary.com/dicom/dicom-tags/
 """
-Verify if the tags exists and the values are correct.
-"""
-def verify_tags(img, metadata):
+def update_image_tags(img, metadata):
+    logger.debug(f"update image tag -> Image - {img}, Metadata - {metadata}")
     ds = pydicom.filereader.dcmread(img)
-    try:
-        assert(ds.PatientName == metadata["patient-name"])  # Patient's Name
-        assert(ds.PatientSpeciesDescription == metadata['request_species'])  # Patient Species Description
-        assert(ds.BarcodeValue == metadata['test_type'])   # Barcode Value
-        assert(ds.InstitutionName == metadata['clinic-name'])  # Institution Name
-        assert(ds.PatientID == metadata['patient_id'])
-        return True
-    except AssertionError as err:
-        logger.error(f"Tag mismatch: {err}")
-        #logger.error(traceback.format_exc())
-        return False
+
+    # Update the tags if needed
+    if ds.PatientName != metadata["patient-name"]:
+        ds.PatientName = metadata["patient-name"]
+
+    if ds.PatientSpeciesDescription != metadata["request_species"]:
+        ds.PatientSpeciesDescription = metadata["request_species"]
+
+    if ds.BarcodeValue != metadata["test_type"]:
+        ds.BarcodeValue = metadata["test_type"]
+
+    if ds.InstitutionName != metadata["clinic-name"]:
+        ds.InstitutionName = metadata["clinic-name"]
+
+    if ds.PatientID != metadata["patient_id"]:
+        ds.PatientID = metadata["patient_id"]
+
+    # Save the changes made to the DICOM file
+    ds.save_as(img)
+    logger.info(f"Tags verified and updated successfully for {img}")
 
 
 """
-Exponential Backoff. This makes a request retry up to attempts set above with exponential backoff.
+Make API requests using exponential backoff.
+This makes a request retry up upto a certain number at exponential interval of time.
+
 https://stackoverflow.com/questions/69855084/retry-with-python-requests-when-status-code-200
 https://medium.com/@roopa.kushtagi/decoding-exponential-backoff-a-blueprint-for-robust-communication-de21459aa98f
 """
-def make_api_request(max_retry, api_type, url, data, headers, backoff_in_seconds=2):
+def make_api_request(max_retry, api_type, url, data, headers, backoff_in_seconds=3):
     attempts = 1
     while True:
         try:
@@ -51,38 +68,45 @@ def make_api_request(max_retry, api_type, url, data, headers, backoff_in_seconds
                 r = requests.get(url, data=data, headers=headers)
             elif (api_type == "post"):
                 r = requests.post(url, data=data, headers=headers)
-            r.status_code == requests.codes.ok
             return r
         except Exception as err:
             logger.warning(err)
             if attempts > max_retry:
                 logger.error("Maximum number of attempts exceeded, aborting.")
                 return False
-                
+
             sleep = backoff_in_seconds * 2 ** (attempts - 1)
             logger.info(f"Retrying request (attempt #{attempts}) in {sleep} seconds...")
             time.sleep(sleep)
             attempts += 1
 
 
-
 """
 Fetch metadata from the given API endpoint
+Configure the API request -> Make API request -> return API response
 """
 def fetch_metadata(sample_id, priority, max_retry):
     url = METADATA_URL
 
-    payload = {"sample_id": sample_id, "priority": priority}
+    payload = {
+            "sample_id": sample_id,
+            "priority": priority
+            }
 
     headers = {
-        "content-type": "application/json", 
+        "content-type": "application/json",
         "x-api-key": METADATA_KEY
         }
-    
-    
-    response = make_api_request(max_retry=max_retry, api_type="get", url=url, data=json.dumps(payload), headers=headers)
-    
-    if (response == False):
+
+    response = make_api_request(
+            max_retry=max_retry,
+            api_type="get",
+            url=url,
+            data=json.dumps(payload),
+            headers=headers
+            )
+
+    if not response:
         return False
     elif (response.status_code == requests.codes.ok):
         logger.info(f"Metadata API -> Received Status Code: {response.status_code}")
@@ -93,81 +117,47 @@ def fetch_metadata(sample_id, priority, max_retry):
         return False
 
 
-
-"""
-Update tags in all dicom images with fetched metadata.
-Read image in directory -> Update tags -> overwrite the existing image with updated tags
-
-Tag References:
-https://dicom.innolitics.com/ciods
-https://www.dicomlibrary.com/dicom/dicom-tags/
-"""
-def update_image_tags(filepath, metadata):
-    # Find all dicom files in the directory
-    get_imgs = list(Path(filepath).rglob("*.[dD][cC][mM]"))
-    
-    # Verify if there are more than one file in the directory
-    if (len(get_imgs) == 0):
-        log.error("No DICOM file in the path provided.")
-        return False
-
-    logger.debug(f"List of imgs found: {get_imgs}")
-    
-    # Update images one by one
-    for img in get_imgs:
-        # Read all tags for image
-        ds = pydicom.filereader.dcmread(img)
-        
-        ds.PatientName = metadata["patient-name"]  # Patient's Name
-        ds.PatientSpeciesDescription = metadata['request_species']  # Patient Species Description
-        ds.BarcodeValue = metadata['test_type']   # Barcode Value
-        ds.InstitutionName = metadata['clinic-name']  # Institution Name
-        ds.PatientID = metadata['patient_id']  # Patient ID
-        
-        # overrite the dcm file with updated tags
-        ds.save_as(img)
-        logger.debug(f"Image updated - {img}")
-
-    logger.info("Successfully updated all images")
-    return True
-
-
-
 """
 Upload all images to given API endpoint.
 Verify tags before uploading.
+Check dicom images in directory -> Configure API request
+-> Update image tags -> upload image
 """
 def upload_imgs(filepath, metadata, max_retry):
     # Find all dicom files in the directory
     get_imgs = list(Path(filepath).rglob("*.[dD][cC][mM]"))
-    
+
     # Verify if there are more than one file in the directory
     if (len(get_imgs) == 0):
-        log.error("No DICOM file in the path provided.")
+        logger.error("No DICOM file in the path provided.")
         return False
-    
+
     logger.debug(f"List of imgs found: {get_imgs}")
-    
+
     # Setup API request parameters
     url = UPLOAD_URL
-    
+
     headers = {
             "content-type": "application/binary",
             "Authorization": UPLOAD_KEY
             }
-    
+
     for img in get_imgs:
-        if (verify_tags(img, metadata) == False):
-            return False
-        
+        update_image_tags(img, metadata)
+
         data = open(img, 'rb').read()
 
-        logger.info(f"Image Upload API -> Uploading {img}")
-        
+        logger.debug(f"Image Upload API -> Uploading {img}")
 
-        response = make_api_request(max_retry=max_retry, api_type="post", url=url, data=data, headers=headers)
-    
-        if (response == False):
+        response = make_api_request(
+                max_retry=max_retry,
+                api_type="post",
+                url=url,
+                data=data,
+                headers=headers
+                )
+
+        if not response:
             return False
         elif (response.status_code == requests.codes.ok):
             logger.info(f"Image Upload API -> Received Status Code: {response.status_code} - Image: {img}")
@@ -175,27 +165,37 @@ def upload_imgs(filepath, metadata, max_retry):
         else:
             logger.error(f"Image Uplaod API -> Received Status Code: {response.status_code}")
             return False
-    
+
     logger.info("Successfully uploaded all images")
     return True
 
 
 """
 Trigger Analytics using the given API endpoint.
+Configure the API request -> Make API request -> return API response
 """
 def trigger_analytics(sample_id, request_type, max_retry):
     url = ANALYTICS_URL
 
-    payload = {"sample_id": sample_id, "request_type": request_type}
+    payload = {
+            "sample_id": sample_id,
+            "request_type": request_type
+            }
 
     headers = {
-        "content-type": "application/json", 
+        "content-type": "application/json",
         "x-api-key": ANALYTICS_KEY
         }
 
-    response = make_api_request(max_retry=max_retry, api_type="get", url=url, data=json.dumps(payload), headers=headers)
-    
-    if (response == False):
+    response = make_api_request(
+            max_retry=max_retry,
+            api_type="get",
+            url=url,
+            data=json.dumps(payload),
+            headers=headers
+            )
+
+    if not response:
         return False
     elif (response.status_code == requests.codes.ok):
         logger.info(f"Analytics API -> Received Status Code: {response.status_code}")
@@ -206,65 +206,65 @@ def trigger_analytics(sample_id, request_type, max_retry):
         return False
 
 
-
 if __name__ == "__main__":
+    # Argument Parser
     parser = argparse.ArgumentParser(description='Moichor Image Upload Script Args')
     parser.add_argument('--sample_id', type=str, default="ref0000022tes-cbc",
                         help='Sample ID (default: ref0000022tes-cbc)')
-    parser.add_argument('--log_level', type=int, default=2,
+    parser.add_argument('--max_retry', type=int, default=3,
                         help='Number of attempts before declaring failure')
-    parser.add_argument('--attempt_number', type=str, default="001",
-                        help='Workaround for "Already Stored" response - PatientID tag [0010, 0020] to: <sample-id>-<attempt-number> (default: False)')
+    parser.add_argument('--attempt_number', type=int, default=1,
+                        help='Workaround for "Already Stored" response - PatientID tag (default: False)')
     parser.add_argument('--directory', type=str, default="./data/",
                         help='Path to the directory where dicom images are stored')
     parser.add_argument('--console_logs', type=bool, default=False,
                         help='Print logs in terminal/stdout (default: False)')
     args = parser.parse_args()
-    
 
-    # Configure logs - write all logs to moichor.log
-    logging.basicConfig(filename='moichor.log',
-                    filemode='w',
+    # Setup log handler - Add stdout if enabled
+    if args.console_logs:
+        log_handler = [
+                logging.FileHandler(f"moichor-{args.sample_id}-{args.attempt_number}.log", mode="w"),
+                logging.StreamHandler(sys.stdout)
+                ]
+    else:
+        log_handler = [
+                logging.FileHandler(f"moichor-{args.sample_id}-{args.attempt_number}.log", mode="w")
+                ]
+
+    # Configure Logs
+    log_level = logging.getLevelName(LOG_LEVEL)
+    logging.basicConfig(
                     format='%(asctime)s | [%(levelname)s] | %(message)s',
                     datefmt='%m/%d/%Y %H:%M:%S',
-                    level=logging.INFO)
-    
-    # Enable stdout for logs
-    if args.console_logs == True:
-        console = logging.StreamHandler()
-        console.setLevel(logging.INFO)
-        # set a format which is the same as log file
-        formatter = logging.Formatter('%(asctime)s | [%(levelname)s] | %(message)s')
-        console.setFormatter(formatter)
-        logging.getLogger("").addHandler(console)
-    
-    logger = logging.getLogger()
+                    level=log_level,
+                    handlers=log_handler
+                    )
 
+    logger = logging.getLogger()
 
     # Fetch Metadata
     logger.info("Fetching Metadata...")
-    md = fetch_metadata(args.sample_id, 2, args.backoff_attempts)
-    if (md == False):
+    md = fetch_metadata(args.sample_id, 2, args.max_retry)
+    if not md:
         logger.error("Exiting -> Unable to fetch metadata")
         sys.exit(1)
-    # PatientID tag [0010, 0020] to: <sample-ID>-<attempt-number>.
-    md["patient_id"] = args.sample_id + "-" + str(args.attempt_number)
 
+    # PatientID tag: <sample-ID>-<attempt-number>.
+    # https://stackoverflow.com/questions/339007/how-do-i-pad-a-string-with-zeros
+    md["patient_id"] = args.sample_id + "-" + str(args.attempt_number).zfill(3)
 
-    # Update Images with metadata tags
-    logger.info("Updating DICOM with metadata...")
-    if (update_image_tags(args.directory, md) == False):
-        logger.error("Exiting -> Unable to update image tags")
-        sys.exit(1)
- 
     # Upload Images to endpoint
     logger.info("Upload images to endpoint...")
-    if (upload_imgs(args.directory, md, args.backoff_attempts) == False):
+    if not (upload_imgs(args.directory, md, args.max_retry)):
         logger.error("Exiting -> Unable to upload images")
         sys.exit(1)
 
     # Trigger Analytics
     logger.info("Trigger Analytics...")
-    analysis = trigger_analytics(args.sample_id, md["test_type"], args.backoff_attempts)
-    logger.info(analysis)
+    analysis = trigger_analytics(args.sample_id, md["test_type"], args.max_retry)
+    if not analysis:
+        logger.error("Exiting -> Unable to trigger analytics")
+        sys.exit(1)
 
+    logger.info(analysis)
